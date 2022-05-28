faa37d0c7c77d268fa6e8440341d32fea78fafb3
tmoris <57036823+tmoris@users.noreply.github.com>
2022-05-28 12:04:07 +0400

    updated main.js and style.css

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,5 @@
-
-
+ const cardButton =  document.querySelector('.card-btn'); // Get element from DOM
+ const ratingCard = document.querySelector('.rating-section');
+ const thanksCard = document.querySelector('.thanks-section');
+ thanksCard.classList.add('invisible');
+div.classList.remove('info') // Remove class "info
