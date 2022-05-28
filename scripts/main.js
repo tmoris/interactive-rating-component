0a820e134959ffeca5b590958eaa89ff211c714e
tmoris <57036823+tmoris@users.noreply.github.com>
2022-05-28 13:16:26 +0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,22 @@
 const cardButton =  document.querySelector('.card-btn'); 
 const ratingCard = document.querySelector('.rating-section');
 const thanksCard = document.querySelector('.thanks-section');
+const rateNumber = document.querySelectorAll('.rates');
 
 //  removing the tahnks card from the DOM
 thanksCard.classList.add('invisible');
 //  adding a click envent listener on the rating card button 
 cardButton.addEventListener('click', () => {
+  rateChoice();
 cardChanger();
 });
 // creating a funtion that changes cards in the DOM
 function cardChanger () {
   ratingCard.classList.add('invisible');
   thanksCard.classList.remove('invisible');
+}
+function rateChoice() {
+rateNumber.addEventListener('click', () => {
+
+});
 }
